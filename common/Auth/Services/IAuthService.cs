0d9b2d7a358ffad664b82e27813854b723055bd2
Nischal Tandia <nischal.tandia@tripxl.com>
2026-07-04 15:31:33 +0530

    fixed warning

--- common/Auth/Services/IAuthService.cs
+++ common/Auth/Services/IAuthService.cs
@@ -2,16 +2,11 @@ using System.Security.Claims;
 using PrePerchaseServer.Modules.Auth.DTOs;
 
 namespace PrePerchaseServer.Modules.Auth.Services;
-
 public interface IAuthService
 {
     Task<AuthResponseDto> LoginAsync(LoginDto dto);
-
     Task<AuthResponseDto> RegisterAsync(RegisterDto dto);
-
     Task<AuthResponseDto> RefreshTokenAsync(RefreshTokenDto dto);
-
-    Task<UserDto> GetCurrentUserAsync(ClaimsPrincipal user);
-
+    Task<UserDto?> GetCurrentUserAsync(ClaimsPrincipal user);
     Task LogoutAsync(ClaimsPrincipal user);
 }
